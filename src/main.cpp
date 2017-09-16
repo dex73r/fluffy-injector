69a3d3a17cd92970fc2c1c57126ab914fc800b00
BuildTools <mojweb123@gmail.com>
2017-09-16 16:44:24 +0200

    injection done

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #define MAX_PATH 260
 #endif
 
-DWORD Process(char const *ProcessName)
+DWORD get_pID(char const *ProcessName)
 {
     HANDLE hPID = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     PROCESSENTRY32 ProcEntry;
@@ -49,25 +49,54 @@ std::string get_last_node(std::string const &p, std::string &output)
     return std::string(p.begin() + i, p.end() - 4); // - 4 because .exe
 }
 
+enum
+{
+    PID_NOT_FOUND = 1,
+    OPENPROCESS_ERROR,
+    LOADLIB_ERROR,
+    ALLOC_ERROR,
+    WPM_ERROR,
+    THREAD_ERROR
+};
+
+unsigned long inject_dll(char *pDll, unsigned long pIDID)
+{
+    void *pProc, *pRemoteThread, *pRemoteBuffer, *pLoadLib;
+    if (!pIDID)
+        return PID_NOT_FOUND;
+    pProc = OpenProcess((PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_OPERATION | PROCESS_VM_WRITE | PROCESS_VM_READ), FALSE, pIDID);
+    if (!pProc)
+        return OPENPROCESS_ERROR;
+    pLoadLib = (void *)GetProcAddress(GetModuleHandle("kernel32.dll"), "LoadLibraryA");
+    if (!pLoadLib)
+        return LOADLIB_ERROR;
+    pRemoteBuffer = VirtualAllocEx(pProc, NULL, strlen(pDll), (MEM_RESERVE | MEM_COMMIT), PAGE_READWRITE);
+    if (!pRemoteBuffer)
+        return ALLOC_ERROR;
+    if (!WriteProcessMemory(pProc, pRemoteBuffer, pDll, strlen(pDll), NULL))
+        return WPM_ERROR;
+    pRemoteThread = CreateRemoteThread(pProc, NULL, 0, (LPTHREAD_START_ROUTINE)pLoadLib, pRemoteBuffer, 0, NULL);
+    if (!pRemoteThread)
+        return THREAD_ERROR;
+    CloseHandle(pProc);
+    return 0;
+}
+
 int main()
 {
     auto path = get_current_path();
-    std::cout << path << "\n";
     if (path.find(".exe") == std::string::npos)
         throw std::runtime_error("Only launch as executable(.exe) supported");
-
     std::string folder;
     auto executable_name = get_last_node(path, folder);
-    DWORD dwProcess;
-    char myDLL[MAX_PATH];
-    /* strncpy(myDLL, dll_path.c_str(), sizeof(myDLL));
-    myDLL[sizeof(myDLL) - 1] = 0; */
-    GetFullPathName(DLL_NAME, MAX_PATH, myDLL, 0);
-    dwProcess = Process("csgo.exe");
-    HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE | PROCESS_VM_OPERATION, FALSE, dwProcess);
-    LPVOID allocatedMem = VirtualAllocEx(hProcess, NULL, sizeof(myDLL), MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    WriteProcessMemory(hProcess, allocatedMem, myDLL, sizeof(myDLL), NULL);
-    CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)LoadLibrary, allocatedMem, 0, 0);
-    CloseHandle(hProcess);
-    return 0;
+    unsigned long pID;
+    char cDLL[MAX_PATH];
+    auto dll_path = folder + executable_name + ".dll";
+    strncpy(cDLL, dll_path.c_str(), sizeof(cDLL));
+    cDLL[sizeof(cDLL) - 1] = 0;
+    pID = get_pID("csgo.exe");
+    unsigned long failure = inject_dll(cDLL, pID);
+    if (failure)
+        std::cout << "error code: " << failure << '\n';
+    return failure;
 }
